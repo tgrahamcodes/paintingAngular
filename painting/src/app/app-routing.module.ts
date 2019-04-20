58a358c2db26975b89c93e455d951b0db68abc05
pattytheirishlad@gmail.com <pattytheirishlad@gmail.com>
2019-04-20 18:41:49 -0400

    first commit

--- painting/src/app/app-routing.module.ts
+++ painting/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+//not needed
+const routes: Routes = [
+/*   {path: '', redirectTo: '/home', pathMatch: 'full'},
+/*
+  {path: 'home', component: HomeComponent },
+  {path: 'services', component: ServicesComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'contact', component: ContactComponent }, */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- painting/src/app/app.module.ts
+++ painting/src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+//app component imports
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavigationComponent } from './nav/navigation/navigation.component';
+
+//material imports
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    ContactComponent,
+    NavigationComponent
   ],
   imports: [
+    MatIconModule,
+    MatTabsModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,NavigationComponent]
 })
 export class AppModule { }
